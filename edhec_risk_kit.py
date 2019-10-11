a1e9dd5f3c22dc7a71b83fda0103f609e03ee46c
Leonardo Rocchi <leonardo.rocchi@yahoo.it>
2019-10-11 16:43:22 +0200

    update toolkit functions

--- edhec_risk_kit.py
+++ edhec_risk_kit.py
@@ -4,7 +4,8 @@ import scipy.stats
 
 def get_ffme_returns():
     '''
-    Returns the French-Fama dataset for the returns of the bottom and top deciles (Low 10 and Hi 10)
+    Returns the French-Fama dataset for the returns of the bottom and top 
+    deciles (Low 10 (Small Caps) and Hi 10 (Large Caps)) of US stocks
     '''
     rets = pd.read_csv("data/Portfolios_Formed_on_ME_monthly_EW.csv", index_col=0, parse_dates=True, na_values=-99.99)
     # Divide by 100, since they are returns, and change the index to datatime
@@ -20,12 +21,11 @@ def get_hfi_returns():
     # Divide by 100, since they are returns, and change the index to datatime
     hfi = hfi / 100
     # the index is already of type datetime
-    #hfi.index = hfi.index.to_period("M") #.to_period("M") forces the index to be monthly period...
     return hfi 
     
 def drawdown(returns: pd.Series):
     '''
-    Computing the Drawdown: takes in input a series of asset returns and returns a dataframe containing 
+    Drawdown: takes in input the returns of an asset and returns a dataframe containing 
     1. the associated wealth index (for an hypothetical investment of $1000) 
     2. all previous peaks 
     3. the drawdowns
@@ -39,7 +39,7 @@ def drawdown(returns: pd.Series):
 def skewness(s):
     '''
     Computes the Skewness of the input Series or Dataframe.
-    There is also the function scipy.stats.skew()
+    There is also the function scipy.stats.skew().
     '''
     return ( ((s - s.mean()) / s.std())**3 ).mean()
 
@@ -53,14 +53,15 @@ def kurtosis(s):
 
 def exkurtosis(s):
     '''
-    Simply return the Excess Kurtosis, i.e., Kurtosis minus 3
+    Returns the Excess Kurtosis, i.e., Kurtosis minus 3
     '''
     return kurtosis(s) - 3
 
 def is_normal(s, level=0.01):
     '''
-    Jarque-Bera test to see if a series (of returns) is normally distributed
-    It returns True or False according to wheter the p value is larger than the level
+    Jarque-Bera test to see if a series (of returns) is normally distributed.
+    Returns True or False according to whether the p-value is larger 
+    than the default level=0.01.
     '''
     statistic, pvalue = scipy.stats.jarque_bera( s )
     return pvalue > level
@@ -72,46 +73,49 @@ def semivolatility(s):
     '''
     return s[s<0].std() 
 
-def var_historic(s, alpha=0.05):
+def var_historic(s, level=0.05):
     '''
-    Returns the (1-alpha)% VaR using historical method. 
-    By default it computes the 95% VaR.
+    Returns the (1-level)% VaR using historical method. 
+    By default it computes the 95% VaR, i.e., alpha=0.95 which gives level 1-alpha=0.05.
     The method takes in input either a DataFrame or a Series and, in the former 
     case, it computes the VaR for every column (Series) by using pd.aggregate
     '''
     if isinstance(s, pd.DataFrame):
-        return s.aggregate( var_historic, alpha=alpha )
+        return s.aggregate( var_historic, level=level )
     elif isinstance(s, pd.Series):
-        return - np.percentile(s, alpha*100)
+        return - np.percentile(s, level*100)
     else:
         raise TypeError("Expected pd.DataFrame or pd.Series")
         
-def var_gaussian(s, alpha=0.05, cf=False):
+def var_gaussian(s, level=0.05, cf=False):
     '''
-    Returns the (1-alpha)% VaR using the parametric Gaussian method. 
-    By default it computes the 95% VaR.
+    Returns the (1-level)% VaR using the parametric Gaussian method. 
+    By default it computes the 95% VaR, i.e., alpha=0.95 which gives level 1-alpha=0.05.
     The variable "cf" stands for Cornish-Fisher. If True, the method computes the 
     modified VaR using the Cornish-Fisher expansion of quantiles.
     The method takes in input either a DataFrame or a Series and, in the former 
     case, it computes the VaR for every column (Series).
     '''
     # alpha-quantile of Gaussian distribution 
-    za = scipy.stats.norm.ppf(alpha,0,1) 
+    za = scipy.stats.norm.ppf(level,0,1) 
     if cf:
         S = skewness(s)
         K = kurtosis(s)
         za = za + (za**2 - 1)*S/6 + (za**3 - 3*za)*(K-3)/24 - (2*za**3 - 5*za)*(S**2)/34    
     return -( s.mean() + za * s.std() )
 
-def cvar_historic(s, alpha=0.05):
+def cvar_historic(s, level=0.05):
     '''
-    Computes the Conditional Var (based on historical method)
+    Computes the (1-level)% Conditional VaR (based on historical method).
+    By default it computes the 95% CVaR, i.e., alpha=0.95 which gives level 1-alpha=0.05.
+    The method takes in input either a DataFrame or a Series and, in the former 
+    case, it computes the VaR for every column (Series).
     '''
     if isinstance(s, pd.DataFrame):
-        return s.aggregate( cvar_historic, alpha=alpha )
+        return s.aggregate( cvar_historic, level=level )
     elif isinstance(s, pd.Series):
         # find the returns which are less than (the historic) VaR
-        mask = s < -var_historic(s, alpha=alpha)
+        mask = s < -var_historic(s, level=level)
         # and of them, take the mean 
         return -s[mask].mean()
     else:
